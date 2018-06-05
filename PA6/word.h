04d4d71e0afc0bdd24fe2eb0842a12c4b8cdd74f
Calvin Le <calvin.md.le@gmail.com>
2018-06-05 12:31:47 -0600

    Add files via upload

---
+++ PA6/word.h
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+void stemAlg(string &word);
+
+bool Vowel(string s, size_t i);
+
+bool Consanant(string s, size_t i);
+
+string Region1(string s);
+
+string Region2(string s);
+
+bool shortSyl(string s);
+
+bool shortWord(string s);
+
+string preceder(string s, size_t i);
+
+void step1(string &word);
+
+void step2(string &word);
+
+void step3(string &word);
+
+void step4(string &word);
+
+void step5(string &word);
+
+void step6(string &word);
+
+void step7(string &word);
+
+void step8(string &word);
+
+bool liEnding(string s);
