9f20ca095566163379aa03f69ac5fa3df3dcc490
Calvin Le <calvin.md.le@gmail.com>
2018-06-05 15:17:35 -0600

    Update count.cpp
    Removed comments

--- PA1/count.cpp
+++ PA1/count.cpp
@@ -3,31 +3,24 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-using namespace std;
 
 void printResults(int numbers[]){
     for (std::size_t c = 0; c < 20; c++){
         std::cout << c << " " << numbers[c] << "\n";
-        //     "x"   " "    "<x counter>"
     }
 }
 
 bool elementCheck(string element){
-    //Rules out decimal/float/double/negatives
     int num;
     for (std::size_t x = 0; x < element.size()-1; x++){
         if (isdigit(int(element[x])) == 0){
-            //cout << "Not a digit!\n";
             return false;
         }
     }
-
-    //confirmed int
     stringstream(element) >> num;
     if ( (num < 0) || (num > 19) ){
         return false;
     }
-
     else
         return true;
 }
