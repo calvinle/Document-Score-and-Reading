7462adc08ed98031aed0105c51afff0e208a7367
Calvin Le <calvin.md.le@gmail.com>
2018-06-08 13:57:14 -0600

    Update main.cpp

--- PA3/main.cpp
+++ PA3/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[]) {
 	ifstream inFile(argv[1]); //Take in input file
 	ifstream excFile(argv[2]); //file of exceptions
 	if (!inFile) {
-		cerr << "Error, file required! LOL\n";
+		cerr << "Error, file required!\n";
 		return -1;
 	}
 	if ( inFile.peek() == std::ifstream::traits_type::eof() ){
@@ -27,7 +27,7 @@ int main(int argc, char* argv[]) {
 		istringstream ss(line);
 		string token;
 
-		while( ss >> token ){	//token separated from white spaces
+		while( ss >> token ){
 			vector<string> sent = tokenSplit(token); //create vector of broken up tokens
 			for (size_t i=0; i < sent.size(); i++){ words.push_back(sent[i]); } //add them all to new vector
 		}
@@ -35,15 +35,6 @@ int main(int argc, char* argv[]) {
 	}
 	
 	vector<string> newWords = capitalCheck(words);
-	
-	/*
-	for (size_t i = 0; i < newWords.size(); i++){
-		if (newWords[i][0] != '+' && isAcro(newWords[i]) == false && 
-		hasDigit(newWords[i]) == false && newWords[i].size() > 2){
-			stemAlg(newWords[i]);
-		}
-	}
-	*/
 	
 	sort(newWords.begin(), newWords.end());
 	if (newWords.empty()) {
