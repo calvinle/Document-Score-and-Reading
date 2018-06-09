d17ea19d2cee5b19a9a95e3e09d86b7548ffb2b1
Calvin Le <calvin.md.le@gmail.com>
2018-06-09 09:58:38 -0600

    Update main.cpp
    Blocks and blocks of comments removed

--- PA6/main.cpp
+++ PA6/main.cpp
@@ -9,16 +9,16 @@
 #include "tfidf.h"
 #include <map>
 #include <iomanip>
-using namespace std;
+
 
 int main(int argc, char* argv[]) {
-	if (argc < 4){ 							//if not enough files
-		cerr << "Need at least 3 files!" << endl;
+	if (argc < 4){
+		std::cerr << "Need at least 3 files!" << endl;
 		return -1;
 	}	
-	ifstream excFile(argv[1]); 				//1st Arg: Exception File. First doc at argv[2]. Last is at argv[argc-1]
-	if (!excFile) {							//if exec. files doesn't exist...
-		cerr << "Error, file(s) required!\n";
+	ifstream excFile(argv[1]);
+	if (!excFile) {
+		std::cerr << "Error, file(s) required!\n";
 		return -1;
 	}
 	
@@ -26,19 +26,19 @@ int main(int argc, char* argv[]) {
 	vector<string> ex1;
 	string line2;
 	if (excFile.peek() != ifstream::traits_type::eof()){ 	//if not empty, parse exceptions prior to Porter
-		while (getline(excFile, line2)) { 					//while not empty line
-			size_t counter = 0; 							//must have 2 words/line. used to check
-			istringstream ss2(line2); 						//capture line
-			string token2;									//token2 intialized
+		while (getline(excFile, line2)) {
+			size_t counter = 0;
+			istringstream ss2(line2);
+			string token2;
 			
-			while( ss2 >> token2 ){ 						//token2 contains word(s separated by space)
-				ex1.push_back(token2); 						//add to vector
-				counter++; 									//increment counter
+			while( ss2 >> token2 ){ 
+				ex1.push_back(token2);
+				counter++;
 			}
 			if (counter == 0){
 				while(getline(excFile, line2)){
 					if (isalnum(line2[0])){
-						cerr << "Error" << endl;
+						std::cerr << "Error" << endl;
 						return -1;
 					}
 					if (excFile.eof()){
@@ -47,11 +47,11 @@ int main(int argc, char* argv[]) {
 				}
 				break;
 			}
-			if (counter != 2){								//if counter not 2, then there aren't exactly 2 words a line
-				cerr << "EXCEPTION FILE: Can't have " << counter << " words on line" << endl;	//throw error
+			if (counter != 2){
+				std::cerr << "EXCEPTION FILE: Can't have " << counter << " words on line" << endl;
 				return -1;
 			}
-			if (excFile.eof()){ break; }					//check for end of file (maybe?)	
+			if (excFile.eof()){ break; }	
 		}
 	}
 	
@@ -61,46 +61,44 @@ int main(int argc, char* argv[]) {
 	for (int i=2; i < argc; i++){
 		map<string, int> doc;
 		ifstream inFile(argv[i]);
-		if ( inFile.peek() == std::ifstream::traits_type::eof()){ //if text file empty
+		if ( inFile.peek() == std::ifstream::traits_type::eof()){
 			cerr << "Error, File " << i - 2 << " empty!\n";
 			return -1;
 		}
 		
 		vector<string> words;
-		string line;											//line to hold each line to parse
-		while (getline(inFile, line)) { 						//while not empty line
+		string line;
+		while (getline(inFile, line)) {
 			istringstream ss(line);
 			string token;
 
-			while( ss >> token ){								//token separated from white spaces
-				vector<string> sent = tokenSplit(token); 		//create vector of broken up tokens
-				for (size_t i=0; i < sent.size(); i++){ words.push_back(sent[i]); } //add them all to new vector
+			while( ss >> token ){
+				vector<string> sent = tokenSplit(token);
+				for (size_t i=0; i < sent.size(); i++){ words.push_back(sent[i]); }
 			}
-		
-			if (inFile.eof()){ break; }							//if end of file
+			if (inFile.eof()){ break; }
 		}
-
-		vector<string> newWords = capitalCheck(words);			//Capitalization Check
+		vector<string> newWords = capitalCheck(words); //Capitalization Check
 		
 		//Check for ambiguous capitals
 		for (size_t i = 0; i < newWords.size(); i++){
-			if (newWords[i][0] == '+'){								 //if properly marked
-			string c = newWords[i].substr(1, newWords[i].size()-1);  //create string var of token being checked without its +
-				for (size_t j=0; j < newWords.size(); j++){ 		 //check rest of document for ambiguous marks
-					if (c == newWords[j] && j != i){	//if found...
-						newWords[i].erase(0,1);						//remove + from word
+			if (newWords[i][0] == '+'){
+			string c = newWords[i].substr(1, newWords[i].size()-1);
+				for (size_t j=0; j < newWords.size(); j++){ 
+					if (c == newWords[j] && j != i){
+						newWords[i].erase(0,1);
 						break;
 					}
-					else if (j == newWords.size()-1 && newWords[i][0] == '+'){ //if reached end of doc and word[i] still unmarked
-						newWords[i].erase(0,1);						//erase +
-						newWords[i][0] += 32;						//convnert to lowercase
+					else if (j == newWords.size()-1 && newWords[i][0] == '+'){
+						newWords[i].erase(0,1);
+						newWords[i][0] += 32;
 					}
 				}
 			}
 		}
 		
 		//Exception File Parse prior to Porter
-		for (size_t i=0; i < newWords.size(); i++){ //scan text file
+		for (size_t i=0; i < newWords.size(); i++){
 			for (size_t j=0; j < ex1.size(); j++){
 				if (newWords[i] == ex1[j] && j % 2 == 0){
 					newWords[i] = ex1[j+1];
@@ -117,44 +115,35 @@ int main(int argc, char* argv[]) {
 			}
 		}
 		
-		//sort, check for empty
 		sort(newWords.begin(), newWords.end());
 		
 		if (newWords.empty()) {
-			cout << "Nothing!" << endl;
+			std::cout << "Nothing!" << endl;
 			return -1;
 		}
 		
 		vector<string> strings;
 		vector<int> counts;
 		for (size_t i=0; i < newWords.size(); i++){
-			int dup = 1;								//First encounter of word = 1
+			int dup = 1;
 			for (size_t j=i+1; j < newWords.size(); j++){
 				if (newWords[i] == newWords[j]){
-					newWords[j].erase();				//if found, delete duplicate
-					dup += 1;							//increment counter, verifying found match
+					newWords[j].erase();
+					dup += 1;
 				}
 			}
 			if (newWords[i] != ""){
-				//cout << "String, int : " << newWords[i] << " " << dup << endl;
 				doc[newWords[i]] = dup;
 			}
 		}
 		c.push_back(doc);
 	}
 	Tfidf lib1(c);
-	/*
-	cout << "Number of docs: " << lib1.N() << endl;
-	cout << "tf, doc2, cow: " << lib1.tf("cow", lib1.doc(2)) << endl;
-	cout << "idf, bird: " << lib1.idf("bird") << endl;
-	*/
 	
-	for (size_t i=0; i < lib1.N(); i++){ //search all docs (n:_)
-		for (size_t j=0; j < lib1.N(); j++){ //docs to compare (_:m)
-			cout << fixed << setprecision(6) << lib1.Sim(lib1.doc(i), lib1.doc(j)) << " ";
+	for (size_t i=0; i < lib1.N(); i++){
+		for (size_t j=0; j < lib1.N(); j++){
+			std::cout << fixed << setprecision(6) << lib1.Sim(lib1.doc(i), lib1.doc(j)) << " ";
 		}
 	}
-	
-	//cout << lib1.Sim(lib1.doc(0), lib1.doc(0)) << " ";
 	return 0;
 }
